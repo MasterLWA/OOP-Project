2bfb99458450ea0df33eedb6a46e0c715acf1040
Binuri Gunawardena <99273958+IT21222122@users.noreply.github.com>
2022-09-09 10:50:15 +0530

    Add files via upload

---
+++ src/main/java/com/javaClasses/leave.java
@@ -0,0 +1,20 @@
+package com.javaClasses;
+
+public class leave {
+	private int empID;
+	private int leaveID;
+	private String reason;
+	private String leave_type;
+	private String approval;
+	//leave_from,leave_till,time
+	private int approvedBy;
+	private int requestBy;
+	
+	
+	public leave() {
+		
+		
+		
+	}
+	
+}
---
+++ src/main/java/com/javaClasses/staff.java
@@ -0,0 +1,25 @@
+package com.javaClasses;
+
+public class staff {
+		private int staffID;
+		private char[] charPassword = new char[20];
+		private String firstName;
+		private String lastName;
+		private char[] p_email = new char[250];
+		private char[] w_email = new char[250];
+		private String gender;
+		private char[] phoneNumber = new char[20];
+		private char[] address = new char[250];
+		//hireDate, quitDate
+		private double salary;
+		private double ot_rate;
+		private char[] staffcol = new char[45];
+		
+
+		public staff() {
+			
+		}
+
+
+
+}
