f484dbd4c6e235f8e7dcbda551c8b6573ea644e6
Lakindu Widuranga Alwis <29312088+MasterLWA@users.noreply.github.com>
2022-11-06 05:54:24 +0530

    Added database connect class to com.admin

---
+++ src/main/java/com/admin/DBconnect.java
@@ -0,0 +1,27 @@
+package com.admin;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DBconnect {
+	        //create database connection
+			private static String url ="jdbc:mysql://localhost:3306/osms"; 
+			private static String user ="root"; 
+			private static String pass = "root1234";
+			private static Connection con;
+			
+			public static Connection getConnection() {
+				try{
+					Class.forName("com.mysql.jdbc.Driver");
+					con = DriverManager.getConnection(url, user, pass);
+					
+					
+				}
+				catch(Exception e){
+					System.out.print("DataBase connection is not successful");
+				}
+				
+				return con;
+			}
+			
+}
